f4100b89fb595ac1c275e79206d665c021d667c1
alogy <1129507496@qq.com>
2019-04-01 23:52:18 +0800

    change yh

--- crawler/zuihaodaxue.py
+++ crawler/zuihaodaxue.py
@@ -22,11 +22,15 @@ def fillUnivList(uList, html):
     for tr in soup.find('tbody', attrs={'class': 'hidden_zhpm'}).children:
         if isinstance(tr, bs4.element.Tag):
             tds = tr('td')
-            uList.append(tds[0].string, tds[1].string, tds[2].string, tds[3].string)
+            uList.append([tds[0].string, tds[1].string, tds[2].string, tds[3].string])
 
 def printUniv(uList, num):
-    print('Suc' + str(num))
-
+    # print('Suc' + str(num))
+    tmpl = '{0:^10}\t{1:{3}^10}}\t{2:^10}\t{3:^10}'
+    print(tmpl.format('排名', '学校名称', '位置', '总分', chr(12288)))
+    for i in range(num):
+        u = uList[i]
+        print(tmpl.format(u[0], u[1], u[2], u[3], chr(12288)))
 
 def main():
     url = 'http://zuihaodaxue.cn/zuihaodaxuepaiming2019.html'
@@ -34,7 +38,7 @@ def main():
     uinfo = []
     text = getHTMLText(url)
     fillUnivList(uinfo, text)
-    printUniv(uinfo, 20)
+    printUniv(uinfo, 5)
 
 
 if __name__ == '__main__':
